bf4fac6c36a22128e0a993ed64c8dfe0fe913722
werunm <cankennethwong@gmail.com>
2025-01-05 10:25:43 -0800

    feat: Added 404 error handelling page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,36 @@
 'use client';
-import Link from 'next/link';
 
-export default function NotFound() {
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+
+const NotFound: React.FC = () => {
+  const router = useRouter();
+
+  const handleBackToHome = () => {
+    router.push('/');
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-6">
-        Oops! The page you're looking for doesn't exist.
-      </p>
-      <Link href="/">
-        <a className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition">
-          Go Back Home
-        </a>
-      </Link>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="md:text-4xl text-2xl font-bold mb-4">
+            404 - Page Not Found
+          </h1>
+          <p className="text-sm md:text-lg mb-6">
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <Button
+            onClick={handleBackToHome}
+            className="px-6 py-3 bg-black text-white rounded-lg hover:bg-slate-50 hover:text-black transition duration-300"
+          >
+            Back to Home
+          </Button>
+        </div>
+      </body>
+    </html>
   );
-}
+};
+
+export default NotFound;
